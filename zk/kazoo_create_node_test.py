13ddc6e5826dc8e120f4dde57fe91718640bf64c
wqzhang <wqzhang0@163.com>
2018-08-21 14:34:07 +0800

    计数用程序

--- zk/kazoo_create_node_test.py
+++ zk/kazoo_create_node_test.py
@@ -1,10 +1,11 @@
 import datetime
+import random
 import threading
 import time
 
 from kazoo.client import KazooClient
 
-from icode.redis.redis_operation_test import redisOperation
+# from icode.redis.redis_operation_test import redisOperation
 
 """
 创建子节点数
@@ -15,38 +16,27 @@ zk = KazooClient(hosts='127.0.0.1:2181')
 ROOT = "/ROOM"
 zk.start()
 
-rds = redisOperation.get_redis()
 index = 0
 
-room_list = list(range(100, 150))
+room_list = list(range(100, 2050))
 room_list = [str(x) for x in room_list]
 
 
 def random_room():
-    return "/RC_NODE"
-    # return random.choice(room_list)
+    # return "/RC_NODE"
+    return random.choice(room_list)
 
 
 if zk.exists(random_room()) is None:
     zk.create(random_room())
 
 print(datetime.datetime.now())
-
-
-def create():
-    zk.create(random_room() + "/", ephemeral=True, sequence=True)
-    room_incr = rds.incr(random_room())
-    if room_incr % 8000 == 0:
-        print(datetime.datetime.now())
-    if room_incr % 200 == 0:
-        for x in range(200):
-            t = threading.Thread(target=create, name="")
-            t.start()
-
-
-for x in range(500):
-    t = threading.Thread(target=create, name="")
-    t.start()
-
+count = 0
+_old = datetime.datetime.now()
 while (True):
-    time.sleep(1000)
+    zk.create(random_room() + "/", ephemeral=True, sequence=True,makepath=True)
+    count = count +1
+    if count % 8000 == 0:
+        now = datetime.datetime.now()
+        print((now -_old).total_seconds())
+        _old = now
