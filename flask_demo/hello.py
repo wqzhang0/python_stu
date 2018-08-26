f0fe5c323a57f31ccea6db9205410d483a52ea1b
wqzhang_home <wqzhang0@163.com>
2018-08-27 00:10:33 +0800

    add flask

--- flask_demo/hello.py
+++ flask_demo/hello.py
@@ -1,11 +1,14 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template, request, redirect, get_flashed_messages, flash
 
 app = Flask(__name__)
 
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/')
 def index():
-    return 'index'
+    flash("You were successfully logged in")
+    return redirect(url_for('login'))
+    # return 'index'
 
 
 @app.route("/")
@@ -15,7 +18,29 @@ def hello_world():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    return "login"
+    def valid_login(name, pwd):
+        if name == "123":
+            return True
+
+    def log_the_user_in(name):
+        return render_template('hello.html', name=name)
+
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'], request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalide username/password'
+
+    return render_template('login.html', error=error)
+
+
+@app.route('/search_title', methods=['GET'])
+def search_title():
+    key = request.args.get('key', '')
+    if key == 'cangjingkong':
+        return render_template('title_list.html', key=key)
+    return render_template('404.html')
 
 
 #
@@ -30,8 +55,30 @@ def prifile(username):
     return '{}\'s profile'.format(username)
 
 
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('login'))
-    print(url_for('login', next='/'))
-    print(url_for('prifile', username='John Doe'))
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post %s' % post_id
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    return 'Subpath %s' % subpath
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template("hello.html", name=name)
+
+
+# with app.test_request_context():
+#     print(url_for('index'))
+#     print(url_for('login'))
+#     print(url_for('login', next='/'))
+#     print(url_for('prifile', username='John Doe'))
+# url_for('static',filename="show_img.jpg")
+
+if __name__ == '__main__':
+    # condition = Condition()
+    app.debug = True
+    app.run()
