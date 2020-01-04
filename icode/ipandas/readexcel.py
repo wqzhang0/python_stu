9caeb799bf9090b96e19f3952c5b34013cc927bd
wqzhang <wqzhang0@163.com>
2020-01-04 15:41:05 +0800

    增加表格读取 和 文件操作

---
+++ icode/ipandas/readexcel.py
@@ -0,0 +1,58 @@
+# from xlrd import open_workbook
+from openpyxl.cell import Cell, MergedCell
+
+secondary_excel_path = "G:\SynologyDrive\文档\人大项目\各种业务导入导出模板\会议信息.xlsx"
+
+from openpyxl import load_workbook
+
+# wb = Workbook(secondary_excel_path)
+wb = load_workbook(secondary_excel_path)
+print(wb.sheetnames)
+
+ws2 = wb.get_sheet_by_name("Sheet1")
+
+if not "Sheet_R" in wb.sheetnames:
+    wb_r = wb.create_sheet("Sheet_R")
+else:
+    wb_r = wb.get_sheet_by_name("Sheet_R")
+print('------------------------------')
+print(ws2.max_column)
+print(ws2.max_row)
+for row in ws2.iter_rows(min_row=1, max_col=3, max_row=200):
+    for cell in row:
+        if isinstance(cell, Cell):
+            print("非合并")
+        elif isinstance(cell, MergedCell):
+            print("合并")
+        else:
+            raise  Exception("sss")
+        print(cell)
+# d2 = ws2.cell(row=4, column=2)
+# print(d2)
+# d1 = ws2.cell(row=4, column=5, value=10)
+# print(d1)
+#
+# print(ws2['A4'])
+# cell_range = ws2['A1':'C2']
+
+
+# wb.save('smp.xlsx')
+# grab the active worksheet
+# ws = wb.active
+#
+# # Data can be assigned directly to cells
+# ws['A1'] = 42
+#
+# # Rows can also be appended
+# ws.append([1, 2, 3])
+#
+# # Python types will automatically be converted
+# import datetime
+# ws['A2'] = datetime.datetime.now()
+#
+# # Save the file
+# ws2 = wb.create_sheet('oh ny zsh',2)
+# ws2.title = 'Nwe Ttitle'
+# ws2.sheet_properties.tabColor = "1072Ba"
+#
+# # wb.save("sample2.xlsx")
